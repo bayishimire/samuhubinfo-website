253db955c55f083e49dd427c9dcce5b2691e56b3
Bayishimire Samuel <bayishimiresamuel760@gmail.com>
2026-02-23 20:09:50 +0200

    Remove secret file

--- SamHub_Info/settings.py
+++ SamHub_Info/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8o!mt%&tj*ng-_g^#@e4od17_voc!qk#nzx#@76(!8uhl*uwud'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,9 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'            # For Gmail
 EMAIL_PORT = 587                          # TLS port
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bayishimiresamuel760@gmail.com'  # Your real email
-EMAIL_HOST_PASSWORD = 'hvtqszcbwordvmyz'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 
 
 
